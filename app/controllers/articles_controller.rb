890c9e19182f15db801f39dc28eb24be1e399d66
Schwad <nicholas.schwaderer@gmail.com>
2015-02-21 02:42:07 +0000

    Create start show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,16 @@
 class ArticlesController < ApplicationController
   def new
   end
+
+  def create
+    @article = Article.new
+    @article.title = params[:title]
+    @article.body = params[:body]
+    @article.save
+    redirect_to article_path(@article.id)
+  end
+
+  def show
+
+  end
 end
